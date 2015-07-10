97f65916a16b9b304faae688c87d60d57f0bccfe
Yahya Zini <yahyazini@gmail.com>
2015-07-10 01:03:29 -0400

    View and Update role permissions list

--- resources/views/admin/role/show.blade.php
+++ resources/views/admin/role/show.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts/admin')
 @section('content')
-<h1>Role: {{ $role->name }}</h1>
-<p>Display Name: {{ $role->display_name }}</p>
-<p>Description: {{ $role->display_name }}</p>
+<h1>Role: {{ $role->display_name }}</h1>
+<p>{{ $role->description }}</p>
 
 <h2>Permissions:</h2>
-<ul>
-    @foreach($role->perms() as $permission)
-        <li>{{ $permission->name }}</li>
+<form action="{{ URL::route('update-role-permissions', $role->id) }}" method="POST">
+    @foreach($allPerms as $singlePerm)
+    <div class="checkbox">
+        <label>
+            <input type="checkbox" name="permissionsCheckBox[]" value="{{ $singlePerm->id}}"
+                @foreach($role->perms as $rolePerm)
+                    @if($rolePerm->id == $singlePerm->id)
+                        checked
+                    @endif
+                @endforeach
+            > {{ $singlePerm->name }}
+        </label>
+    </div>
     @endforeach
-</ul>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <button type="submit" class="btn btn-default">Update Permissions</button>
+</form>
 @stop

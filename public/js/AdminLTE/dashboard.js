82c40c026e84b494ad1babb9b764bcf75c345f59
Yahya Zini <yahyazini@gmail.com>
2015-07-12 04:29:26 -0400

    Integrating DataTable for table lists

--- public/js/AdminLTE/dashboard.js
+++ public/js/AdminLTE/dashboard.js
@@ -16,6 +16,7 @@ $(function() {
         forcePlaceholderSize: true,
         zIndex: 999999
     }).disableSelection();
+
     $(".box-header, .nav-tabs").css("cursor","move");
     //jQuery UI sortable for the todo list
     $(".todo-list").sortable({
@@ -29,21 +30,22 @@ $(function() {
     $(".textarea").wysihtml5();
 
     $('.daterange').daterangepicker(
-            {
-                ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                    'Last 7 Days': [moment().subtract('days', 6), moment()],
-                    'Last 30 Days': [moment().subtract('days', 29), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-                },
-                startDate: moment().subtract('days', 29),
-                endDate: moment()
+        {
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                'Last 7 Days': [moment().subtract('days', 6), moment()],
+                'Last 30 Days': [moment().subtract('days', 29), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
             },
-    function(start, end) {
-        alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    });
+            startDate: moment().subtract('days', 29),
+            endDate: moment()
+        },
+        function(start, end) {
+            alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    );
 
     /* jQueryKnob */
     $(".knob").knob();
--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -1,58 +1,54 @@
 @extends('layouts/dashboard')
 @section('content')
-<h1>roles List</h1>
-<div class="input-group col-lg-4 pull-right">
-    <input type="search" class="form-control light-table-filter" data-table="order-table" aria-describedby="search" placeholder="Filter roles">
-    <span class="input-group-addon" id="search"><i class="glyphicon glyphicon-search"></i></span>
-</div>
-<div class="">
-    <a href="{{ URL::route('admin.role.create') }}" class="btn btn-default">Add a role</a>
-</div>
+<h1>Roles List</h1>
 
-<table class="table table-striped sorted_table order-table">
-    <thead>
-        <tr>
-            <th style="display:none;">ID</th>
-            <th style="display:none;">Sort</th>
-            <th>Name</th>
-            <th>Display Name</th>
-            <th>Description</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody class="list">
-        @foreach ($roles as $role)
-        <tr>
-            <td style="display:none;" class="id">{{ $role->id }}</td>
-            <td style="display:none;" class="sort">{{ $role->sort }}</td>
-            <td class="roleName">{{ $role->name }}</td>
-            <td class="roleName">{{ $role->display_name }}</td>
-            <td>{{ $role->description }}</td>
-            <td>
-                <a href="{{ URL::route('admin.role.edit', $role->id ) }}">
-                    <span class="glyphicon glyphicon-edit"></span> Edit Role
-                </a><br>
+<div class="box">
+    <div class="box-header">
+        <h3 class="box-title">Roles List</h3>
+    </div><!-- /.box-header -->
+    <div class="box-body table-responsive">
+        <table class="table table-bordered table-striped paginatedDataTable">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($roles as $role)
 
-{{--
-                <a href="{{ URL::route('admin.role.show', $role->id ) }}">
-                    <span class="glyphicon glyphicon-lock"></span> View Role Permissions
-                </a><br>
---}}
-
-                <a href="{{ URL::route('admin.role.show', $role->id ) }}">
-                    <span class="glyphicon glyphicon-lock"></span> Edit Role Permissions
-                </a><br>
-
-                <form action="{{ URL::route('admin.role.destroy', $role->id ) }}" method="POST">
-                    <input type="hidden" name="_method" value="DELETE">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <button type="submit" ><span class="glyphicon glyphicon-remove"></span> Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+                    <tr>
+                        <td>{{ $role->id }}</td>
+                        <td>{{ $role->name }}</td>
+                        <td>{{ $role->description }}</td>
+                        <td>
+                            <div class="btn-group-vertical block">
+                                <a href="{{ URL::route('admin.role.show', $role->id) }}" class="btn btn-default">
+                                    <span class="glyphicon glyphicon-eye-open"></span> View
+                                </a>
+                                <a href="{{ URL::route('admin.role.edit', $role->id) }}" class="btn btn-default">
+                                    <span class="glyphicon glyphicon-edit"></span> Edit
+                                </a>
+                                <a href="{{ URL::route('admin.role.destroy', $role->id) }}" class="btn btn-default">
+                                    <span class="glyphicon glyphicon-remove"></span> Delete</a>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div><!-- /.box-body -->
+</div><!-- /.box -->
 <div class="pull-right">
     <a href="{{ URL::route('admin.role.create') }}" class="btn btn-default">Add a role</a>
 </div>

24a6e49e3a092f1c37bb177cd47d5871540db518
Yahya Zini <yahyazini@gmail.com>
2015-07-12 12:07:20 -0400

    Cleaning up routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,25 +17,21 @@
 Route::group(['prefix' => 'admin', "namespace"=>"Admin"], function(){
 
     Route::get('/',  "DashboardController@index");
-    Route::get("dashboard", array("as"=>"admin-dashboard", "uses"=>"DashboardController@index"));
 
+    Route::get("dashboard", array("as"=>"admin-dashboard", "uses"=>"DashboardController@index"));
+    Route::get("login", array("as"=>"admin-login", "uses"=>"AuthController@create"));
     Route::resource('role', 'RoleController');
     Route::resource('permission', 'PermissionController');
     Route::resource('user', 'UserController');
     Route::resource('auth', 'AuthController', ['only' => ['create', 'store', 'destroy']]);
 
-
-
-
     Route::group(array("before"=>"csrf"), function(){
         Route::post('role/updatePermissions/{role_id}', array("as" => "update-role-permissions", "uses" => 'RoleController@updatePermissions'));
     });
 
-    Route::get("login", array("as"=>"admin-login", "uses"=>"AuthController@create"));
-    Route::group(array('before' => 'auth'), function(){
-    });
 
 });
+
 /*Public Routes*/
 Route::get('user/register', array("as"=>"register", "uses"=> "UserController@create"));
 Route::resource('user', 'UserController');

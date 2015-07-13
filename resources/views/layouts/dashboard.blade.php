9101b6677cdef4038f692a1d6a5c109381e8688b
Yahya Zini <yahyazini@gmail.com>
2015-07-12 23:16:37 -0400

    Add Page Title and page description to dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -50,8 +50,8 @@
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        Dashboard
-                        <small>Control panel</small>
+                        {{ $page_title or "Page Title" }}
+                        <small>{{ $page_description or null }}</small>
                     </h1>
                     <ol class="breadcrumb">
                         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
